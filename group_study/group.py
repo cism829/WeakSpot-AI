f42de60ab15ddb46e2e83804f0e6aae881d92a48
cism829 <clrodgers@uncg.edu>
2025-09-18 13:00:46 -0400

    multichat room logic

--- group_study/group.py
+++ group_study/group.py
@@ -10,23 +10,29 @@ templates = Jinja2Templates(directory="templates")
 
 class ConnectionManager:
     def __init__(self):
-        self.active_connections: list[WebSocket] = []
+        self.active_connections: dict[str, list[WebSocket]] = {}
     
-    async def connect(self, websocket: WebSocket):
+    async def connect(self, websocket: WebSocket, room: str):
         await websocket.accept()
-        self.active_connections.append(websocket)
+        if room not in self.active_connections:
+            self.active_connections[room] = []
+        self.active_connections[room].append(websocket)
         return True
 
-    def disconnect(self, websocket: WebSocket):
-        self.active_connections.remove(websocket)
+    def disconnect(self, websocket: WebSocket, room: str):
+        if room in self.active_connections:
+            self.active_connections[room].remove(websocket)
+            if not self.active_connections[room]:
+                del self.active_connections[room]
 
     async def send_personal_message(self, message: str, websocket: WebSocket):
         await websocket.send_text(message)
 
-    async def broadcast(self, message: str, sender: WebSocket):
-        for connection in self.active_connections:
-            if connection is not sender:
-                await connection.send_text(message)
+    async def broadcast(self, message: str, room: str, sender: WebSocket = None):
+        if room in self.active_connections:
+            for connection in self.active_connections[room]:
+                if connection is not sender:
+                    await connection.send_text(message)
 
 
 manager = ConnectionManager()
@@ -37,16 +43,16 @@ async def get(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
 
 
-@app.websocket("/ws/{client_id}")
-async def websocket_endpoint(websocket: WebSocket, client_id: int):
-    await manager.connect(websocket)
-    await manager.broadcast(f"User #{client_id} has entered the chat!", websocket)
+@app.websocket("/ws/{room}/{client_id}")
+async def websocket_endpoint(websocket: WebSocket, room: str, client_id: int):
+    await manager.connect(websocket, room)
+    await manager.broadcast(f"User #{client_id} has entered {room}!", room)
     try:
         while True:
             data = await websocket.receive_text()
             await manager.send_personal_message(f"You: {data}", websocket)
-            await manager.broadcast(f"User #{client_id} says: {data}", websocket)
+            await manager.broadcast(f"User #{client_id} says: {data}", room, websocket)
     except WebSocketDisconnect: 
-        manager.disconnect(websocket)
-        await manager.broadcast(f"Client #{client_id} has left the chat")
+        manager.disconnect(websocket, room)
+        await manager.broadcast(f"User #{client_id} has left {room}", room, sender=websocket)
 
