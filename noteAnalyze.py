a1e0587690317713e7587d1cb9a692e8f11db2bb
cism829 <clrodgers@uncg.edu>
2025-09-22 12:17:54 -0400

    preprocessing issues

--- noteAnalyze.py
+++ noteAnalyze.py
@@ -7,13 +7,15 @@ warnings.filterwarnings("ignore", category=UserWarning)
 from transformers import logging
 logging.set_verbosity_error()
 
+import spacy
+
 
 
 # model_name = "t5-base"
 # tokenizer = T5Tokenizer.from_pretrained(model_name)
 # model = T5ForConditionalGeneration.from_pretrained(model_name)
 
-print('model')
+print('importing model')
 model_name = "google/flan-t5-large"
 tokenizer = AutoTokenizer.from_pretrained(model_name)
 model = AutoModelForSeq2SeqLM.from_pretrained(model_name)
@@ -24,18 +26,34 @@ ai_text = []
 example1 = "example_notes/example1.pdf"
 example2 = "example_notes/example2.pdf"
 
+ai_output = "ai_results/ai_fixed_notes0"
+
+nlp = spacy.load("en_core_web_sm")
+
 print("extracting file")
-with pdfplumber.open(example2) as myPdf:
+with pdfplumber.open(example1) as myPdf:
     for page in myPdf.pages:
         original_text += page.extract_text() + "\n"
 
-# print(original_text)
+print(original_text)
 print('proccessing notes')
 notes = []
 for line in original_text.split("\n"):
     line = line.strip()
     if line != "":
         notes.append(line)
+
+# def preprocess(text):
+#     doc = nlp(text)
+#     processed_lines = []
+#     for sentence in doc.sents:
+#         line = sentence.text.strip()
+#         if line:
+#             processed_lines.append(line)
+#     return processed_lines
+
+# notes = preprocess(original_text)
+
 
 def incomplete_def(line):
     line = line.strip()
@@ -45,6 +63,12 @@ def incomplete_def(line):
         return True 
     
     return False
+    # line = line.strip()
+    # # consider it incomplete if it's short (<5 words) and has no period
+    # if len(line.split()) <= 5 and not line.endswith("."):
+    #     print("Incomplete:", line)
+    #     return True
+    # return False
     
 
 def whole_def(word):
@@ -102,10 +126,6 @@ Rules:
 Notes:
 {text[:3000]}
 """
-
-
-
-
     
     input_ids = tokenizer.encode(prompt, return_tensors="pt", truncation=True)
 
@@ -152,10 +172,10 @@ print("Summary:\n", summary)
 
 print('writing to file')
 
-with open("ai_notes2.txt", "w", encoding="utf-8") as ai_note:
+with open(ai_output, "w", encoding="utf-8") as new_note:
     for line in ai_text:
-        ai_note.write(line + "\n")
-    ai_note.write('\nSummary \n')
-    ai_note.write(summary)
+        new_note.write(line + "\n")
+    new_note.write('\nSummary \n')
+    new_note.write(summary)
 
 print("done")
